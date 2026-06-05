1e42adff9c399ed9cf1e91d78fda648a4202c4df
Abdelrahman Mohammad <57917560+aboodmo@users.noreply.github.com>
2026-06-04 23:43:07 -0400

    initial commit

---
+++ Ghostpad/GhostpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostpadApp.swift
+//  Ghostpad
+//
+//  Created by Abdelrahman Mohammad on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GhostpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
